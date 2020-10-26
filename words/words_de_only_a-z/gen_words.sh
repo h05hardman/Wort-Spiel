2a591452ef578935874f4079dd270417e963745c
Joshix <joshix.dev@gmail.com>
2020-10-26 22:25:18 +0100

    remove files in .gitignore
    git rm --cached `git ls-files -i --exclude-from=.gitignore`

--- words/words_de_only_a-z/gen_words.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-#script to generate the word files.
-
-while read line; do
-    word="${line,,}"
-    echo "$word" >> "${#word}.txt"
-    echo "$word: ${#word}"
-done < "full_wordlist.txt" #read from full_wordlist.txt (a file with all words, one in each line)
